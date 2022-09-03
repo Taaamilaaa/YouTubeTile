538289b42a0e7e15e9be4cc483061876d665fbc1
Yudina Tamila <79252896+Taaamilaaa@users.noreply.github.com>
2022-09-03 19:07:01 +0300

    adds autoplay

--- js/index.js
+++ js/index.js
@@ -3,39 +3,39 @@ const tileList = document.querySelector('.tile__list');
 const videoPath = [
     {
         id: '1',
-        url: 'https://www.youtube.com/embed/OZbH5MdMRSs',
+        url: 'https://www.youtube.com/embed/OZbH5MdMRSs?autoplay=1',
     },
     {
         id: '2',
-        url: 'https://www.youtube.com/embed/yJcCKuxfb2o',
+        url: 'https://www.youtube.com/embed/yJcCKuxfb2o?autoplay=1',
     },
     {
         id: '3',
-        url: 'https://www.youtube.com/embed/ESITUo-WX1c',
+        url: 'https://www.youtube.com/embed/ESITUo-WX1c?autoplay=1',
     },
     {
         id: '4',
-        url: 'https://www.youtube.com/embed/LPdvfRzVoAg',
+        url: 'https://www.youtube.com/embed/LPdvfRzVoAg?autoplay=1',
     },
     {
         id: '5',
-        url: 'https://www.youtube.com/embed/mX_jE18Zuyg',
+        url: 'https://www.youtube.com/embed/mX_jE18Zuyg?autoplay=1',
     },
     {
         id: '6',
-        url: 'https://www.youtube.com/embed/9EZ9D7Fns9g',
+        url: 'https://www.youtube.com/embed/9EZ9D7Fns9g?autoplay=1',
     },
     {
         id: '7',
-        url: 'https://www.youtube.com/embed/jgLiQfXZtmo',
+        url: 'https://www.youtube.com/embed/jgLiQfXZtmo?autoplay=1',
     },
     {
         id: '8',
-        url: 'https://www.youtube.com/embed/m_6IZJ5LSmY',
+        url: 'https://www.youtube.com/embed/m_6IZJ5LSmY?autoplay=1',
     },
     {
         id: '9',
-        url: 'https://www.youtube.com/embed/1myXPWLj2ew',
+        url: 'https://www.youtube.com/embed/1myXPWLj2ew?autoplay=1',
     },
 ];
 
@@ -65,7 +65,7 @@ function addVideo(element) {
 
     videoPath.forEach(el => {
         if (el.id === id) {
-            const iframe = `<iframe src=${el.url} id = ${id} width="290" frameborder="0" allowfullscreen ></iframe>`;
+            const iframe = `<iframe src=${el.url.concat('?autoplay=1')} id = ${id} width="290" frameborder="0" allow="accelerometer;autoplay;encrypted-media;gyroscope;picture-in-picture" allowfullscreen=""></iframe>`;
             element.parentNode.innerHTML = iframe;
             el.url = element.src;
         }
